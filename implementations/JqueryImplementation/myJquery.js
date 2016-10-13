cea3615889b78350fc1ce2f737400ffb86b11c3e
acilsd <acilsd@derpy.ru>
2016-10-13 20:49:57 +0300

    html(), val(), text() implementation

--- implementations/JqueryImplementation/myJquery.js
+++ implementations/JqueryImplementation/myJquery.js
@@ -1,6 +1,19 @@
 (function() {
-  //our own jquery object
-  var $ = function(selector) {};
+
+  var $ = function(selector) {
+    if( !(this instanceof $) ) {
+      return new $(selector);
+    }
+
+    var elements = document.querySelectorAll(selector);
+    // NOTE: bad way
+    for (var i = 0; i < elements.length; i++) {
+      this[i] = elements[i];
+    }
+    this.length = elements.length;
+    //NOTE: good way
+    //Array.prototype.push.call(this, elements);
+  };
   //extend method implementation
   $.extend = function(target, object) {
     for (var prop in object) {
@@ -62,8 +75,77 @@
     //proxy method implementation
     proxy: function(fn, context) {
       return function() {
-        fn.apply(context, arguments);
+        return fn.apply(context, arguments);
       };
+    },
+    // NOTE: holy shit
+    //DOM methods
+    find: function() {
+
     }
+  });
+  /*****/
+  //html manipulations
+  /*****/
+  $.extend($.prototype, {
+    html: function(newHtml) {
+      if(arguments.length) {
+        $.each(this, function(e, el) {
+          el.innerHTML = newHtml;
+        });
+        return this;
+      } else {
+        return this[0] && this[0].innerHTML;
+      }
+    },
+    val: function(newVal) {
+      if(arguments.length) {
+        $.each(this, function(e, el) {
+          el.value = newVal;
+        });
+        return this;
+      } else {
+        return this[0] && this[0].value;
+      }
+    },
+    text: function(newText) {
+      if(arguments.length) {
+        return $.each(this, function(i, el) {
+          el.innerHTML = '';
+          var text = document.createTextNode(newText);
+          el.appendChild(text);
+        });
+      }
+    },
+    find: function(selector) {
+
+    },
+    next: function() {
+
+    },
+    prev: function() {
+
+    },
+    parent: function() {
+
+    },
+    children: function() {
+
+    },
+    attr: function() {
+
+    },
+    css: function() {
+
+    },
+    width: function() {
+
+    },
+    offset: function() {
+
+    },
+    hide: function() {
+
+    },
   });
 });
