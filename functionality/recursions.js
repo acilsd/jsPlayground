60e3be78c2921f1dde9c31a4a0d8d4783a96a98c
acilsd <acilsd@derpy.ru>
2016-11-19 06:35:37 +0300

    eslint gone crazy :3

--- functionality/recursions.js
+++ functionality/recursions.js
@@ -27,21 +27,21 @@ function sumarizerRecursive() {
 console.log(sumarizerRecursive(3, 5, 6, 7, 8));
 
 const es6RecursiveSum = (...args) => {
-  if (args.length <= 2) { return args[0] + args[1] };
+  if (args.length <= 2) { return args[0] + args[1]; }
   return (
     args[0] + es6RecursiveSum(...args.slice(1))
   );
-}
+};
 
 console.log(es6RecursiveSum(3, 5, 6, 7, 8));
 
 const recursiveMult = (...args) => {
-  if (args.length === 0) { return 0 }
-  if (args.length <= 1) { return args[0] }
-  if (args.length <= 2) { return args[0] * args[1] }
+  if (args.length === 0) { return 0; }
+  if (args.length <= 1) { return args[0]; }
+  if (args.length <= 2) { return args[0] * args[1]; }
   return (
     args[0] * recursiveMult(...args.slice(1))
   );
-}
+};
 
 console.log(recursiveMult(2, 5, 6));
